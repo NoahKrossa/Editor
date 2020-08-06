af1d7def746374e3cf83cd60434bff792336c140
noah krossa <noahkrossa@gmail.com>
2020-08-06 14:17:52 +0200

    remove hello component and create document component

---
+++ src/components/document.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="document">ss</div>
+</template>
+<script>
+  export default {
+    name: 'document',
+    props: []
+  }
+</script>
+<style scoped>
+  #document {
+    width: 800px;
+    margin: 0 auto;
+    background-color: white;
+    min-height: 100vh;
+  }
+</style>
