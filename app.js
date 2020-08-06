9d70c4bb371af28f189188818c50176e8e4d435b
noah krossa <noahkrossa@gmail.com>
2020-08-06 20:41:24 +0200

    Add functionality for toolbar hide/show buttons and prevent enter keydown event into doc

--- app.js
+++ app.js
@@ -1,24 +1,34 @@
 const doc = document.getElementById("document-body");
 const buttons = document.querySelectorAll("[data-tag]");
+const buttonToggle = document.querySelector("[data-toggle]");
 
 const insertNode = (tagname) => {
-  let node = document.createElement(tagname)
-  node.contentEditable = true
-  node.addEventListener('keyup', (e)=> {
-    if(e.code === 'Backspace' && node.textContent.trim()==="")
-      doc.removeChild(node)
-  })
-  doc.appendChild(node)
+  let node = document.createElement(tagname);
+  node.contentEditable = true;
+  node.addEventListener("keydown", (e) => {
+    if (e.code === "Backspace" && node.textContent.trim() === "")
+      doc.removeChild(node);
+      if (e.code === "Enter"){
+        return e.preventDefault()
+      }
+  });
+  doc.appendChild(node);
+  buttonToggle.dataset.toggle = 'false'
 };
 
-buttons.forEach(btn => {
-  btn.addEventListener('click', (e)=> {
-    insertNode(btn.dataset.tag)
-  })
-})
+buttonToggle.addEventListener("click", (e) => {
 
+  if(buttonToggle.dataset.toggle ==='false') buttonToggle.dataset.toggle = 'true'
+  else buttonToggle.dataset.toggle = 'false'
 
+});
 
 // APP ENTRY
-const main = () => {};
+const main = () => {
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      insertNode(btn.dataset.tag);
+    });
+  });
+};
 window.addEventListener("load", main);
