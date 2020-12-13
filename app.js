32a9fa38d9ed8ac2e7ffc85e6fcddd38e8813365
Noah Krossa <noahkrossa@gmail.com>
2020-12-13 12:28:57 +0100

    Add documentation and refactor propsbar controller
    however this featur is not aviable for now

--- app.js
+++ app.js
@@ -3,22 +3,32 @@ const buttons = document.querySelectorAll("[data-tag]");
 const buttonToggle = document.querySelector("[data-toggle]");
 const commandButtons = document.querySelectorAll("[data-command]");
 
+/**
+ * this function create and insert a html element into document
+ * @param {string} tagname
+ */
 const insertNode = (tagname) => {
   let node = document.createElement(tagname);
   node.contentEditable = true;
+
   node.addEventListener("keydown", (e) => {
+    /** Remove node if user type backspace and node is empty*/
     if (e.code === "Backspace" && node.textContent.trim() === "")
       doc.removeChild(node);
     if (e.code === "Enter") {
       return e.preventDefault();
     }
   });
+
   node.addEventListener("focus", (e) => {
-  if(node.tagName === 'P') document.getElementById('propbar').style.display = 'block'
-  else document.getElementById('propbar').style.display = 'none'
-  })
+    if (node.tagName === "P")
+      document.getElementById("propbar").style.display = "block";
+    else document.getElementById("propbar").style.display = "none";
+  });
 
   doc.appendChild(node);
+
+  /** to hide insertable options */
   buttonToggle.dataset.toggle = "false";
 };
 
@@ -43,25 +53,12 @@ const main = () => {
   });
 
   // propsbar controller
-  commandButtons.forEach((btn) => {
-    btn.addEventListener("click", (e) => {
-      updateCommandButtonState(btn);
-      switch (btn.dataset.command) {
-        case "bold":
-          document.execCommand("bold");
-          break;
-        case "italic":
-          document.execCommand("italic");
-          break;
-        case "underline":
-          document.execCommand("underline");
-          break;
-        default:
-          document.execCommand("strikethrough");
-          break;
-      }
-    });
-  });
+  // commandButtons.forEach((btn) => {
+  //   btn.addEventListener("click", (e) => {
+  //     updateCommandButtonState(btn);
+  //     document.execCommand(btn.dataset.command);
+  //   });
+  // });
 };
 
 window.addEventListener("load", main);
